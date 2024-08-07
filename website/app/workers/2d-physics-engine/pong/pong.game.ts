d257bbc266c64b9e9afa08eb91de3f837e818c20
Nándor Dudás <nandor.dudas@gmail.com>
2024-08-07 19:18:21 +0200

    fix: missing direction

--- website/app/workers/2d-physics-engine/pong/pong.game.ts
+++ website/app/workers/2d-physics-engine/pong/pong.game.ts
@@ -1,9 +1,10 @@
 import type { Paddle } from './bodies/paddle'
+import type { Direction } from '../constants'
 
 import { Game } from '../core/game/game'
 
-export class PongGame<T extends Record<string, unknown>, Events extends Record<string, unknown>> extends Game<T, Events> {
-  movePaddle(paddle: Paddle, direction: 'up' | 'down'): void {
+export class PongGame<T extends Record<string, any>, Events extends Record<string, unknown>> extends Game<T, Events> {
+  movePaddle(paddle: Paddle, direction: Direction): void {
     debug('movePaddle', paddle, direction)
   }
 }
