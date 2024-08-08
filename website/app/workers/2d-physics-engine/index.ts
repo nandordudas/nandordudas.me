afe8a23cc84cad2cbf7dbb7f7be80a40fc373064
Nándor Dudás <nandor.dudas@gmail.com>
2024-08-08 10:49:03 +0200

    docs: revert unnecessary comments

--- website/app/workers/2d-physics-engine/index.ts
+++ website/app/workers/2d-physics-engine/index.ts
@@ -112,6 +112,7 @@ eventBus.on('setup', (offscreenCanvas) => {
 
 /**
  * {@link https://developer.mozilla.org/en-US/docs/Web/API/Worker/error_event|Worker: message event}
+ *
  * @example
  * addEventListener('message', messageEventHandler)
  */
@@ -121,6 +122,7 @@ export function messageEventHandler({ data }: MessageEvent<Events>): void {
 
 /**
  * {@link https://developer.mozilla.org/en-US/docs/Web/API/Worker/message_event|Worker: error event}
+ *
  * @example
  * addEventListener('error', errorHandler)
  */
