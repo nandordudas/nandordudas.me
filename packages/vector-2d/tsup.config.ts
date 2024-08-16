d902831cafa4eab57e234de868d69608c76a6e8b
Nándor Dudás <nandor.dudas@gmail.com>
2024-08-16 11:47:03 +0200

    chore(vector-2d): update package scripts

--- packages/vector-2d/tsup.config.ts
+++ packages/vector-2d/tsup.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: [
+    'src/index.ts',
+    'src/utils.ts',
+    'src/vector-2d.ts',
+  ],
   splitting: false,
   sourcemap: true,
   clean: true,
   target: 'es2022',
   minify: true,
+  format: 'esm',
+  dts: true,
 })
